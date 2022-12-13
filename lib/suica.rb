c5ba787cc699467066eaf80dba37df624eac8ac9
Give <papanda.5656@outlook.com>
2022-12-13 20:45:27 +0900

    fix: suicaにチャージするためのコードを実装

--- lib/suica.rb
+++ lib/suica.rb
@@ -1,1 +1,29 @@
+require 'pry'
 
+class Suica 
+  def initialize
+    @have_money = 0
+  end
+
+  def chage_money
+    puts "suicaのチャージ残高は#{@have_money}円です"
+    puts "100円以上の金額をチャージしてください"
+
+    money = gets.to_i
+    
+    if money >= 100
+      @have_money = money
+      puts "#{@have_money}円チャージしました!"
+
+    else 
+      until money >= 100
+        puts "注意: 100円以上の金額をチャージしてください!"
+        money = gets.to_i
+        if money >= 100
+          @have_money = money 
+          puts "#{@have_money}円チャージしました!"
+        end
+      end
+    end
+  end
+end
