032c29e65704438b0838cac94c7efcbeaee6cfd3
Give <papanda.5656@outlook.com>
2023-01-08 22:41:26 +0900

    利用者属性管理のためにメソッドを定義

--- lib/suica.rb
+++ lib/suica.rb
@@ -2,10 +2,11 @@ require 'pry'
 require_relative 'user'
 
 class Suica 
-  attr_reader :have_money :user_list
+  attr_reader :have_money ,:suica_entry ,:user_age ,:user_gender
 
   def initialize
     @have_money = 0
+    @suica_entry = []
   end
 
   def chage_money
@@ -30,25 +31,92 @@ class Suica
     end
   end
 
-  def user
-    @user_list = []
+  def entry_user
     puts "suicaが未登録です"
     puts "ユーザー登録をしましょう。"
 
     puts "年齢を入力してください"
-    age_input = gets.to_i
+    age= gets.to_i
+
     #suicaの利用は公式では１２歳からなので12歳以上１００歳までを条件指定。
-    until age_input >=12 && age_input <= 100
-      puts "年齢は12歳以上でないと登録できません"
-      puts "もう一度入力してください。"
+    if age >= 12 && age <= 100
+      puts "次に性別を１か２で選択してください"
 
-      age_input = gets.to_i
+      gender_list = ["男性","女性"]
+      gender_list.each.with_index(1){|n, i|
+        puts "#{i}. #{n}"
+      }
+      select_gender = {1 => "男性", 2 => "女性"}
 
-      if age_input >= 12 && age_input <= 100
+      choice= gets.to_i
+
+      if choice <= 2
+        user = User.new(age,select_gender[choice])
+        @user_age = user.age
+        @user_gender = user.gender
+        @suica_entry << user
+
+        puts "#{user.age}歳 #{user.gender} で登録が完了しました"
+      else
+        until choice <= 2
+          puts "注意:性別は「１」または「２」で半角数字で入力してください"
+          choice = gets.to_i
+          
+          if choice <= 2
+            user = User.new(age,select_gender[choice])
+            @user_age = user.age
+            @user_gender = user.gender
+            @suica_entry << user
+    
+            puts "#{user.age}歳 #{user.gender} で登録が完了しました"
+          end
+        end
+      end
+    else
+      until age >=12 && age <= 100
+        puts "注意:年齢は12歳以上でないと登録できません"
+        puts "もう一度入力してください。"
+
+        age = gets.to_i
+
+        if age >= 12 && age <= 100
+          puts "次に性別を１か２で選択してください"
+    
+          gender_list = ["男性","女性"]
+          gender_list.each.with_index(1){|n, i|
+          puts "#{i}. #{n}"
+          }
+
+          select_gender = {1 => "男性", 2 => "女性"}
+          choice= gets.to_i
+          
+          if choice <= 2
+            user = User.new(age,select_gender[choice])
+            @user_age = user.age
+            @user_gender = user.gender
+            @suica_entry << user
+    
+            puts "#{user.age}歳 #{user.gender} で登録が完了しました"
+          else
+            until choice <= 2
+              puts "注意:性別は「１」または「２」で半角数字で入力してください"
+              choice = gets.to_i
+              
+              if choice <= 2
+                user = User.new(age,select_gender[choice])
+                @user_age = user.age
+                @user_gender = user.gender
+                @suica_entry << user
         
-
+                puts "#{user.age}歳 #{user.gender} で登録が完了しました"
+              end
+            end
+          end
+        end
       end
     end
+
+
   end
 end
 
