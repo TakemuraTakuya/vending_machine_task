fde27de417716883d97a34849db947861dd7d916
Give <papanda.5656@outlook.com>
2023-01-09 10:52:36 +0900

    refactor: userの年齢と性別を変数に格納

--- lib/main.rb
+++ lib/main.rb
@@ -6,10 +6,14 @@ require_relative "vending_machine"
 suica = Suica.new
 
 suica.entry_user
+
+user_age = suica.user_age
+user_gender = suica.user_gender
+
 suica.chage_money
 money = suica.have_money
 
-machine = Vending_Machine.new(money: money)
+machine = Vending_Machine.new(money: money, user_age: user_age, user_gender: user_gender)
 
 #購入できる商品のメニュー画面を表示
 machine.display
@@ -25,6 +29,5 @@ machine.display
 machine.purchase
 
 #売上の管理
-machine.sale_log
-
+pp machine.sale_log
 
